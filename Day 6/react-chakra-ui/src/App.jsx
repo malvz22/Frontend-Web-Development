6068fd75e90afa972b21634b083bbdc1875bf831
Malvin A <malvinaudriansyah77@gmail.com>
2023-11-22 12:12:21 +0700

    Exercise
    Finishing Exercise

--- Day 6/react-chakra-ui/src/App.jsx
+++ Day 6/react-chakra-ui/src/App.jsx
@@ -1,46 +1,82 @@
+/* eslint-disable react/prop-types */
 import {
   Box,
   Heading,
-  Stack,
   Flex,
   Spacer,
   Text,
   Checkbox,
+  Center,
+  Divider,
+  Input,
+  Button,
+  Stack,
 } from "@chakra-ui/react";
 import { DeleteIcon } from "@chakra-ui/icons";
 import "./App.css";
 
-function List() {
+function List(props) {
   return (
-    <Stack width={600} mx="auto">
-      <Checkbox defaultChecked color="white">
-        <Flex>
-          <Text>Test</Text>
+    <Center mb={10}>
+      <Box width="700px">
+        <Flex direction="row" justifyContent={"center"}>
+          <Checkbox color="white" marginEnd={12} size={"lg"}></Checkbox>
+          <Text color={"white"} alignSelf={"center"}>
+            {props.text}
+          </Text>
           <Spacer />
-          <DeleteIcon alignSelf="center"></DeleteIcon>
+          <Center
+            cursor={"pointer"}
+            borderRadius={"10px"}
+            border={"2px solid #FC8181"}
+            width={12}
+            height={12}
+          >
+            <DeleteIcon
+              boxSize={"40%"}
+              color={"#FC8181"}
+              alignSelf={"center"}
+            />
+          </Center>
         </Flex>
-      </Checkbox>
-    </Stack>
+      </Box>
+    </Center>
   );
 }
 
 function App() {
   return (
     <>
-      <Box bg="blue.900" py={10} textAlign="center">
-        <Heading textColor="white" my={12}>
+      <Box bg="#1A202C" py={10}>
+        <Heading textColor="white" textAlign={"center"} py={10}>
           Chores ToDo List
         </Heading>
-        <List></List>
-        <Flex>
-          <Box p="4" bg="red.400">
-            Box 1
-          </Box>
-          <Spacer />
-          <Box p="4" bg="green.400">
-            Box 2
-          </Box>
-        </Flex>
+
+        <List text="Create Guest Experience mobile check in" />
+        <List text="Document current CI/CD process" />
+        <List text="Perform Code review for final Pillow-talk release" />
+        <List text="Fix image uploading process for guest check-in" />
+        <List text="Provide on-boarding documentation" />
+
+        <Divider />
+
+        <Heading textColor="white" textAlign={"center"} py={10}>
+          Done: 0
+        </Heading>
+        <Center>
+          <Stack>
+            <Text color={"white"}>Add todo</Text>
+            <Input
+              placeholder="Test"
+              _placeholder={{ color: "white" }}
+              size="lg"
+              width={"700px"}
+            />
+            <Button color={"black"} colorScheme="blue" width={40}>
+              ADD TASK
+            </Button>
+          </Stack>
+        </Center>
       </Box>
     </>
   );
